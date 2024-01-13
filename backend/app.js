975002ccdece2fd1baeff009441c5d52c557322c
fintech user <kumar.tanmay21@gmail.com>
2024-01-13 18:41:16 +0530

    sending react from node

--- backend/app.js
+++ backend/app.js
@@ -5,13 +5,9 @@ const cors = require("cors");
 const path = require("path");
 require("dotenv").config();
 
-// middleware
-const corsOptions = {
-    origin: "https://fintech-react.onrender.com/"
-}
 app.use(express.json());
 app.use(cors());
-// connect MongoDB
+
 mongoose.connect(process.env.MONGODB_URI).then(() => {
     const PORT = process.env.PORT || 8000
     app.listen(PORT, () => {
@@ -21,24 +17,14 @@ mongoose.connect(process.env.MONGODB_URI).then(() => {
     console.log(err);
 });
 
-app.use(express.static(path.join(__dirname, "../fintech/public")));
-// app.use((req, res, next) => {
-//     console.log("req", req.headers)
-//     next()
-// })
-// route
-
+app.use(express.static(path.join(__dirname, "../fintech/build")));
 
 app.get("/user", (req, res) => {
     res.status(201).send({ message: "Connected to user!" });
 });
 
-console.log("path", path.join(__dirname, "../../fintech/build", "index.html"))
+console.log("path", path.join(__dirname, "../../fintech/build/index.html"))
 
 app.get("*", (req, res) => {
-    console.log(__dirname)
-    console.log(__dirname, '../')
-    console.log(__dirname, '../../')
-    console.log(path.join(__dirname, "../fintech//build/robots.txt"))
-    res.sendFile(path.join(__dirname, "../fintech/public/index.html"));
+    res.sendFile(path.join(__dirname, "../fintech/build/index.html"));
 });
